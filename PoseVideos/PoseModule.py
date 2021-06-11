cbda33dc63832cb04681e0501b48f799c9bff0e3
mike.bskim <mike.bskim@gmail.com>
2021-06-11 15:46:06 +0200

    AI trainer

--- PoseVideos/PoseModule.py
+++ PoseVideos/PoseModule.py
@@ -3,6 +3,7 @@
 import cv2
 import mediapipe as mp
 import time
+import math
 
 
 class poseDetector():
@@ -33,19 +34,48 @@ class poseDetector():
 
   def findPosition(self, img, draw=True):
 
-    lmList = []
+    self.lmList = []
     if self.results.pose_landmarks:
       for id, lm in enumerate(self.results.pose_landmarks.landmark):
         h, w, c = img.shape
       # print(id, lm, h, w, c)
         cx, cy = int(lm.x * w), int(lm.y * h)
-        lmList.append([id, cx, cy])
+        self.lmList.append([id, cx, cy])
         if draw:
           cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
 
-    return lmList
+    return self.lmList
 
 
+  def findAngle(self, img, p1, p2, p3, draw=True):
+
+      # Get the landmarks
+      x1, y1 = self.lmList[p1][1:]
+      x2, y2 = self.lmList[p2][1:]
+      x3, y3 = self.lmList[p3][1:]
+
+      # Calculate the Angle
+      angle = math.degrees(math.atan2(y3 - y2, x3 - x2) -
+                            math.atan2(y1 - y2, x1 - x2))
+      if angle < 0:
+          angle += 360
+
+      # print(angle)
+
+      # Draw
+      if draw:
+          cv2.line(img, (x1, y1), (x2, y2), (255, 255, 255), 3)
+          cv2.line(img, (x3, y3), (x2, y2), (255, 255, 255), 3)
+          # 10짜리 붉은색 채운 원, 15자리 붉은색 라인 원
+          cv2.circle(img, (x1, y1), 10, (0, 0, 255), cv2.FILLED)
+          cv2.circle(img, (x1, y1), 15, (0, 0, 255), 2)
+          cv2.circle(img, (x2, y2), 10, (0, 0, 255), cv2.FILLED)
+          cv2.circle(img, (x2, y2), 15, (0, 0, 255), 2)
+          cv2.circle(img, (x3, y3), 10, (0, 0, 255), cv2.FILLED)
+          cv2.circle(img, (x3, y3), 15, (0, 0, 255), 2)
+          cv2.putText(img, str(int(angle)), (x2 - 70, y2 + 50),
+                      cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+      return angle
 
 
 def main():
