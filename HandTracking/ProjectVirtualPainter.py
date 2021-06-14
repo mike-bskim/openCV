bbaf96fe3bb69bc418baa07659627ef373300fc9
mike.bskim <mike.bskim@gmail.com>
2021-06-14 16:39:36 +0200

    virtual painter

--- HandTracking/ProjectVirtualPainter.py
+++ HandTracking/ProjectVirtualPainter.py
@@ -30,6 +30,7 @@ cap.set(4, 720)
 
 detector = htm.handDetector(detectionCon=0.65, maxHands=1)
 xp, yp = 0, 0
+
 imgCanvas = np.zeros((720, 1280, 3), np.uint8)
 
 while True:
@@ -39,7 +40,7 @@ while True:
     img = cv2.flip(img, 1)  # 1=left/right, 0=up/down
 
     # 2. Find Hand Landmarks
-    img = detector.findHands(img, False)
+    img = detector.findHands(img)
     lmList = detector.findPosition(img, draw=False)
 
     if len(lmList) != 0:
@@ -56,7 +57,7 @@ while True:
 
         # 4. If Selection Mode - Two finger are up
         if fingers[1] and fingers[2]:
-            # xp, yp = 0, 0
+            xp, yp = 0, 0
             print("Selection Mode")
             # # Checking for the click
             if y1 < 125:
@@ -73,7 +74,7 @@ while True:
                     header = overlayList[3]
                     drawColor = (0, 0, 0)
             # cv2.rectangle(img, (x1, y1 - 25), (x2, y2 + 25), drawColor, cv2.FILLED)
-            cv2.line(img, (x1, y1), (x2, y2), drawColor, 10)
+            cv2.line(img, (x1, y1), (x2, y2), drawColor, 15)
 
         # 5. If Drawing Mode - Index finger is up
         if fingers[1] and fingers[2] == False:
@@ -82,33 +83,31 @@ while True:
             if xp == 0 and yp == 0:
                 xp, yp = x1, y1
 
-            cv2.line(img, (xp, yp), (x1, y1), drawColor, brushThickness)
+            # cv2.line(img, (xp, yp), (x1, y1), drawColor, brushThickness)
 
-            # if drawColor == (0, 0, 0):
-            #     cv2.line(img, (xp, yp), (x1, y1), drawColor, eraserThickness)
-            #     cv2.line(imgCanvas, (xp, yp), (x1, y1), drawColor, eraserThickness)
-            #
-            # else:
-            #     cv2.line(img, (xp, yp), (x1, y1), drawColor, brushThickness)
-            #     cv2.line(imgCanvas, (xp, yp), (x1, y1), drawColor, brushThickness)
+            if drawColor == (0, 0, 0):
+                cv2.line(img, (xp, yp), (x1, y1), drawColor, eraserThickness)
+                cv2.line(imgCanvas, (xp, yp), (x1, y1), drawColor, eraserThickness)
+
+            else:
+                cv2.line(img, (xp, yp), (x1, y1), drawColor, brushThickness)
+                cv2.line(imgCanvas, (xp, yp), (x1, y1), drawColor, brushThickness)
 
             xp, yp = x1, y1
 
-
-        # # Clear Canvas when all fingers are up
-        # if all (x >= 1 for x in fingers):
-        #     imgCanvas = np.zeros((720, 1280, 3), np.uint8)
+        # Clear Canvas when all fingers are up
+        if all (x >= 1 for x in fingers):
+            imgCanvas = np.zeros((720, 1280, 3), np.uint8)
 
     imgGray = cv2.cvtColor(imgCanvas, cv2.COLOR_BGR2GRAY)
     _, imgInv = cv2.threshold(imgGray, 50, 255, cv2.THRESH_BINARY_INV)
-    imgInv = cv2.cvtColor(imgInv,cv2.COLOR_GRAY2BGR)
-    img = cv2.bitwise_and(img,imgInv)
-    img = cv2.bitwise_or(img,imgCanvas)
-
+    imgInv = cv2.cvtColor(imgInv, cv2.COLOR_GRAY2BGR)
+    img = cv2.bitwise_and(img, imgInv)
+    img = cv2.bitwise_or(img, imgCanvas)
 
     # Setting the header image
     img[0:125, 0:1280] = header
-    # img = cv2.addWeighted(img,0.5,imgCanvas,0.5,0)
+    # img = cv2.addWeighted(img, 0.5, imgCanvas, 0.5, 0)
     cv2.imshow("Image", img)
     # cv2.imshow("Canvas", imgCanvas)
     # cv2.imshow("Inv", imgInv)
